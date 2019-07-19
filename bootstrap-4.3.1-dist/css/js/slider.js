1c18226cd95c7f5cbbfb0d60d1b8a9ceb76df4ab
Andrey <andgorod4kov5@gmail.com>
2019-07-19 14:45:06 +0300

    Revert "й"
    This reverts commit 6612d793765d07964742cddb7ea8b71cc3abc988.

---
+++ bootstrap-4.3.1-dist/css/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slideshow').slick({
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+      autoplaySpeed: 3000,
+      arrows:false,
+    });
+});
